216b41312415d85562bd8ab0fd3a24950525ba55
choilm <choilmto@gmail.com>
2019-03-03 23:36:19 -0500

    Implement input text
    Added a new stateful component to keep track of
    hard-coded fields, which are then mapped to
    pdf fields.

--- src/components/VLAvailableFields.jsx
+++ src/components/VLAvailableFields.jsx
@@ -1,30 +1,41 @@
 import React from 'react'
-import PdfFieldsDisplay from './PdfFieldsDisplay'
-import CsvFieldsDisplay from './CsvFieldsDisplay'
+import VLFieldMappingInput from './VLFieldMappingInput'
 
-export default class VLAvailableFields extends React.Component {
-  render() {
-    const containerStyle = {
-      gridArea: '2 / 1 / span 1 / span 2',
-      display: 'grid',
-      gridTemplateColumns: `${this.props.width} ${this.props.width}`
-    }
-
-    return (
-      <div style={containerStyle}>
-        <PdfFieldsDisplay pdfFields={this.props.pdfFields}/>
-        <div style={{fontSize: '10em',
-          display: this.props.pdfFields.length === 0 ? 'initial' : 'none'}}>1</div>
-        <CsvFieldsDisplay
-          csvFields={this.props.csvFields}
-          pdfFields={this.props.pdfFields}
-          empty={this.props.empty}
-          table={this.props.table}
-          width={this.props.width}
-          onFieldMappingChange={this.props.onFieldMappingChange}/>
-        <div style={{fontSize: '10em',
-          display: this.props.csvFields.length === 0 ? 'initial' : 'none'}}>2</div>
-      </div>
-    );
+const VLAvailableFields = (props) => {
+  const containerStyle = {
+    gridArea: '2 / 1 / span 1 / span 2',
+    display: 'grid',
+    gridTemplateColumns: `${props.width} ${props.width}`
   }
+
+  return (
+    <div style={containerStyle}>
+      <div style={{display: props.pdfFields.length === 0 ? 'none' : 'grid'}}>
+        {props.pdfFields.map(pdfElement =>
+          <div key={`${pdfElement.fieldName}Div1`}
+            style={{overflow: "auto"}}>{pdfElement.fieldName}
+          </div>)}
+      </div>
+      <div style={{fontSize: '10em',
+        display: props.pdfFields.length === 0 ? 'initial' : 'none'}}>1</div>
+      <div style={{display: props.csvFields.length === 0 ? 'none' : 'grid'}}>
+        {props.csvFields.length === 0 ? [] :
+          props.pdfFields.map(pdfElement =>
+            <VLFieldMappingInput
+              key={`${pdfElement.fieldName}Div2`}
+              table={props.table}
+              text={props.text}
+              empty={props.empty}
+              width={props.width}
+              csvFields={props.csvFields}
+              pdfFields={props.pdfFields}
+              pdfElement={pdfElement}
+              onFieldMappingChange={props.onFieldMappingChange}/>)}
+      </div>
+      <div style={{fontSize: '10em',
+        display: props.csvFields.length === 0 ? 'initial' : 'none'}}>2</div>
+    </div>
+  )
 }
+
+export default VLAvailableFields
---
+++ src/components/VLFieldMappingInput.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default class VLFieldMappingInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.mapInput = this.mapInput.bind(this)
+    this.editCustomValue = this.editCustomValue.bind(this)
+    this.submitCustomValue = this.submitCustomValue.bind(this)
+    this.updateFieldValue = this.updateFieldValue.bind(this)
+    this.state = {
+      canEdit: true,
+      isEditingCustomValue: false,
+      fieldValue : this.props.pdfElement.fieldValue
+    }
+  }
+
+  editCustomValue() {
+    this.setState({isEditingCustomValue: true})
+  }
+
+  submitCustomValue(event) {
+    this.setState(prevState => ({isEditingCustomValue: false,
+      fieldValue: prevState.fieldValue}))
+  }
+
+  mapInput(event) {
+    this.setState({canEdit: event.target.options.selectedIndex === 0})
+    this.props.onFieldMappingChange(
+      event.target.value,
+      this.props.pdfElement.fieldName,
+      (event.target.options.selectedIndex === 0) ?
+        this.props.text : this.props.table)
+  }
+
+  updateFieldValue(event) {
+    if(event.key == 'Enter') {
+      this.submitCustomValue(event)
+      return
+    }
+    this.setState({fieldValue: event.target.value})
+  }
+
+  render() {
+    const options = this.props.csvFields.map(csvElement =>
+      <option value={csvElement.fieldName}
+        key={`${csvElement.fieldName}Option`}>{csvElement.fieldName}</option>)
+
+    return (
+      <div>
+        <select
+          onChange={this.mapInput}
+          style={{maxWidth: '13em',
+          display: this.state.isEditingCustomValue ? 'none' : 'initial'}}>
+          <option value={this.state.fieldValue}>{this.state.fieldValue === ''
+            ? this.props.empty : `Text: "${this.state.fieldValue}"`}</option>
+          {options}
+        </select>
+        <input type="text" style={{maxWidth: '13em',
+          display: this.state.isEditingCustomValue ? 'initial' : 'none'}}
+          onChange={this.updateFieldValue}
+          onKeyPress={this.updateFieldValue}></input>
+        <button style={{width: '2em',
+          display: !this.state.isEditingCustomValue &&
+            this.state.canEdit ? 'initial' : 'none'}}
+          onClick={this.editCustomValue}>p</button>
+        <button style={{width: '2em',
+          display: this.state.isEditingCustomValue ? 'initial' : 'none'}}
+          onClick={this.submitCustomValue}>ok</button>
+      </div>
+    )
+  }
+}
