96d413a99f31263d0f5e2c4a6c8f63fcf74a3d5b
choilm <choilmto@gmail.com>
2019-05-21 14:25:21 -0400

    Applied formatting changes as suggested by linter for VLFieldMappingInput.jsx, except for type validation
    Also adjusted VLHeader.jsx for consistency.

--- src/components/VLFieldMappingInput.jsx
+++ src/components/VLFieldMappingInput.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const VLFieldMappingInput = (props) => {
   const options = props.csvFields.map(csvElement =>
-    <option value={csvElement.columnNumber}
-      key={`${csvElement.columnNumber}-col`}>{csvElement.fieldName}</option>)
+    (
+      <option
+        value={csvElement.columnNumber}
+        key={`${csvElement.columnNumber}-col`}
+      >
+        {csvElement.fieldName}
+      </option>
+    ),
+  );
 
-  const selectHandler = event => {
-    const selectedIndex = event.target.options.selectedIndex
+  const selectHandler = (event) => {
+    const selectedIndex = event.target.options.selectedIndex;
     props.setFieldMapping(
       props.index,
       {
         selectedIndex: selectedIndex,
         state: {
-                 selectedIndex: selectedIndex,
-                 canEdit: selectedIndex === 0
-               }
-      }
-    )
-  }
+          selectedIndex: selectedIndex,
+          canEdit: selectedIndex === 0,
+        },
+      },
+    );
+  };
 
   const submitCustomValue = () => {
     props.setFieldMapping(
@@ -25,66 +32,99 @@ const VLFieldMappingInput = (props) => {
       {
         selectedIndex: 0,
         state: {
-                 isEditingCustomValue: false
-               }
-      }
-    )
-  }
+          isEditingCustomValue: false,
+        },
+      },
+    );
+  };
 
-  const inputHandler = event => {
-    if(event.key == 'Enter') {
-      submitCustomValue()
-      return
+  const inputHandler = (event) => {
+    if (event.key === 'Enter') {
+      submitCustomValue();
+      return;
     }
     props.setFieldMapping(
       props.index,
       {
         state: {
-                 fieldValue: event.target.value
-               }
-      }
-    )
-  }
+          fieldValue: event.target.value,
+        },
+      },
+    );
+  };
 
   const changeToEdit = () =>
     props.setFieldMapping(
       props.index,
       {
         state: {
-                 isEditingCustomValue: true
-               }
-      }
-    )
+          isEditingCustomValue: true,
+        },
+      },
+    );
 
   return (
     <div>
       <select
-        onChange={selectHandler}
         value={props.state.selectedIndex}
-        style={{maxWidth: '13em',
-        height: '1.5em',
-        display: props.state.isEditingCustomValue ? 'none' : 'initial'}}>
-        <option value={props.state.fieldValue}>{props.state.fieldValue === ''
-          ? "<empty>" : `Text: "${props.state.fieldValue}"`}</option>
+        onChange={selectHandler}
+        style={{
+          maxWidth: '13em',
+          height: '1.5em',
+          display: props.state.isEditingCustomValue
+            ? 'none'
+            : 'initial',
+        }}
+      >
+        <option
+          value={props.state.fieldValue}
+        >
+          {props.state.fieldValue === ''
+            ? '<empty>'
+            : `Text: "${props.state.fieldValue}"`}
+        </option>
         {options}
       </select>
-      <input type="text" style={{width: '12.75em',
-        height: '1em',
-        display: props.state.isEditingCustomValue ? 'initial' : 'none'}}
+      <input
+        type="text"
         value={props.state.fieldValue}
         onChange={inputHandler}
-        onKeyPress={inputHandler}></input>
-      <button style={{width: '2em',
-        display: props.state.isEditingCustomValue ? 'none' : 'initial',
-        height: '1.5em'}}
+        onKeyPress={inputHandler}
+        style={{
+          width: '12.75em',
+          height: '1em',
+          display: props.state.isEditingCustomValue
+            ? 'initial'
+            : 'none',
+        }}
+      />
+      <button
         disabled={!props.state.canEdit}
-        onClick={changeToEdit}>p</button>
-      <button style={{width: '2em',
-        height: '1.5em',
-        display: props.state.isEditingCustomValue ? 'initial' : 'none'}}
-        onClick={submitCustomValue}>ok</button>
+        onClick={changeToEdit}
+        style={{
+          width: '2em',
+          height: '1.5em',
+          display: props.state.isEditingCustomValue
+            ? 'none'
+            : 'initial',
+        }}
+      >
+        p
+      </button>
+      <button
+        onClick={submitCustomValue}
+        style={{
+          width: '2em',
+          height: '1.5em',
+          display: props.state.isEditingCustomValue
+            ? 'initial'
+            : 'none',
+        }}
+      >
+        ok
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default VLFieldMappingInput
+export default VLFieldMappingInput;
--- src/components/VLHeader.jsx
+++ src/components/VLHeader.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const VLHeader = () =>
   (
-    <div style={{ backgroundColor: 'grey' }}>
-      <h1 style={{ textAlign: 'center' }}>Filling PDF form fields</h1>
+    <div
+      style={{ backgroundColor: 'grey' }}
+    >
+      <h1
+        style={{ textAlign: 'center' }}
+      >
+        Filling PDF form fields
+      </h1>
     </div>
   );
 
